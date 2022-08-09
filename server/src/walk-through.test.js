03bfdbb9b619f406a4a201846738deaaa474f65c
Oleksandra Koshchii <oleksandra.koshchii@mrstudios.eu>
2022-08-09 13:45:43 +0200

    added another test

--- server/src/walk-through.test.js
+++ server/src/walk-through.test.js
@@ -5,3 +5,9 @@ test('basic example with 23 input', () => {
     const expectedResults = [ "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
     expect(walkThroughCharTree(str).sort()).toEqual(expectedResults.sort());
 });
+
+test('find a real word - APPLE', () => {
+    const str = '27753';
+    const expectedWord = 'apple';
+    expect(walkThroughCharTree(str)).toEqual(expect.arrayContaining([expectedWord]));;
+})
